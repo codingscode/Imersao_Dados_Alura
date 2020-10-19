d5f2a20fdb907c4d006676817034d0becbcee7eb
Heronnes Pereira <heronnestg@hotmail.com>
2020-10-19 12:41:36 -0300

    parte 1

--- aula1.py
+++ aula1.py
@@ -1,7 +1,16 @@
 
-a = 'valor1'
+import pandas as pd
 
-print(a)
+#fonte = 'https://github.com/alura-cursos/imersao-dados-2-2020/blob/master/MICRODADOS_ENEM_2019_SAMPLE_43278.csv?raw=true'
+# forma alternativa se já baixado 
+fonte = './nao_enviar/MICRODADOS_ENEM_2019_SAMPLE_43278.csv'
+
+
+pd.set_option('display.max_columns', None)
+
+
+dados = pd.read_csv(fonte)
+print(dados.head())
 
 
 
